8c97730368c9ffc2ab6e692ac75c5896bcfcf105
Osamahaleem <116170317+Osamahaleem@users.noreply.github.com>
2022-10-19 17:00:20 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,80 @@
+<?php
+$con = mysqli_connect("localhost","root","","pagination");
+$per_page = 5;
+$start = 0;
+$current_page = 1;
+if(isset($_GET['start'])){
+    $start = $_GET['start'];
+    if($start<=0){
+    $start=0;
+    $current_page = 1;
+    }
+    else{
+    $current_page = $start;
+    $start--;
+    $start = $start*$per_page;
+    }
+     
+   }
+$record = mysqli_num_rows(mysqli_query($con,"select * from pages"));
+$page = ceil($record/$per_page);
+if($start<$page){
+
+}
+
+$sql = "select * from pages limit $start, $per_page";
+$res = mysqli_query($con,$sql);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+
+<section class="col-lg-12">
+   <?php
+   if(mysqli_num_rows($res)>0){
+
+   while($row = mysqli_fetch_assoc($res)){
+    ?>
+    <div class="col-md-6 bg-red"><?php echo $row['title']?></div><br>
+    <?php
+   }} else {
+    echo "<h3>No Record Found</h3>";
+   }?>
+   
+</section>
+
+<nav aria-label="...">
+  <ul class="pagination">
+  <?php 
+  
+  // for print no of pages
+  for($i=1;$i<=$page;$i++){ 
+    $class="";
+    //for active class
+   if($current_page == $i){
+    $class='active';
+   }
+    
+    ?>
+   <li><a class="page-link" <?php echo $class;?> href="?start=<?php echo $i;?>"><?php echo $i;?></a></li>
+   <?php } ?>
+   
+    
+    
+  </ul>
+</nav>
+</body>
+</html>
---
+++ pagination.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 19, 2022 at 01:39 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pagination`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pages`
+--
+
+CREATE TABLE `pages` (
+  `id` int(11) NOT NULL,
+  `title` varchar(230) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pages`
+--
+
+INSERT INTO `pages` (`id`, `title`) VALUES
+(1, 'record 1'),
+(2, 'record 2'),
+(3, 'record 3'),
+(4, 'record 4'),
+(5, 'record 5'),
+(6, 'record 6');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pages`
+--
+ALTER TABLE `pages`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pages`
+--
+ALTER TABLE `pages`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
